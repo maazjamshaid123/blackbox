d3612066ed13a59dd0f56ee1236ee2690206de76
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-03-07 11:58:32 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from blackhole import detect_hole
 from intro import show_intro
 
-st.set_page_config(page_title = 'Black Box | AstroAlgo',page_icon="page_icon.png", layout="wide", menu_items={
+st.set_page_config(page_title = 'Black Box | AstroAlgo',page_icon="page_icon.jpeg", layout="wide", menu_items={
         'Get Help': 'https://www.linkedin.com/company/astroalgo',
         'Report a bug': "https://forms.gle/5hKrGPJGqs2aeHUC8",
         'About': '''$Odyssey$ by [AstroAlgo](https://www.linkedin.com/company/astroalgo): Data-Driven Insights Into The Cosmos'''
